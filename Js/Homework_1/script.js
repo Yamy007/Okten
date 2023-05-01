bb5fba3a39c837b3ea5055521834c718ff0005d8
Yamy007 <yamy19007@gmail.com>
2023-05-01 23:06:59 +0200

    remove

---
+++ Js/Homework_1/script.js
@@ -0,0 +1,60 @@
+let hello = "hello"
+
+let owu = "owu"
+
+let com = "com"
+
+let ua = "ua"
+
+let num1 = 1
+
+let num2 = 10
+
+let num3 = -999
+
+let num4 = 123
+
+let num5 = 3.14
+
+let num6 = 2.7
+
+let num7 = 16
+
+let bool1 = true
+
+let bool2 = false
+
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(num7)
+console.log(bool1)
+console.log(bool2)
+
+let firstName = "Yaroslav"
+let middleName = "Sadovskyi"
+let lastName = "Oleksandrovych"
+let person = firstName + " " + middleName + " " + lastName
+
+console.log(person)
+
+let a = 100;
+let b = '100'; 
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+let name = prompt("What is your name?")
+let surname = prompt("What is your surname?")
+let age = prompt("How old are you?")
+let person1 = name + " " + surname + " " + age
+
+console.log(person1)
